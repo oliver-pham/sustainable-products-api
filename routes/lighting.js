1caac696c0f7588e591ddc76a502fc7a0fc83f2d
Phu Thong Pham <ptpham4@myseneca.ca>
2021-05-08 21:38:29 -0400

    Added limit to GET request

--- routes/lighting.js
+++ routes/lighting.js
@@ -2,34 +2,31 @@ require("dotenv").config();
 var express = require('express');
 var axios = require('axios');
 var router = express.Router();
-var paginate = require('../utils/paginate')
 
 /**
  * GET /lighting?
  * 
  * @param technology "LED" (light emitting diode), "CFL" (compact fluorescent light ) or "halogen"
  * @param page_size the number of products fetched per page
- * @param page_number the page number
  * @public
  */
 router.get("/", (req, res) => {
   axios({
     method: "GET",
     url: process.env.API_URL + "wyt9-72bp.json",
+    params: {
+      $limit: req.query.page_size,
+    },
     headers: {
       "X-App-Token": process.env.API_TOKEN,
     },
   })
     .then((response) => {
-      // TODO:  Always "LED"?
-      const products = response.data
-        .filter(
-          (fixture) => fixture.lighting_technology_used === req.query.technology
-        );
-
-      res.json(
-        paginate(products, req.query.page_size, req.query.page_number),
+      const products = response.data.filter(
+        (fixture) => fixture.lighting_technology_used === "LED"
       );
+        
+      res.json(products);
     })
     .catch((err) => {
       res.status(404).json({ message: err.toString() });
